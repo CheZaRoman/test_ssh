2e09103d8c07ceedebd702df567dd1fee69b6604
Roman <chirka@dlit.dp.ua>
2021-03-17 20:43:22 +0200

    source.py updated

--- source.py
+++ source.py
@@ -17,14 +17,14 @@ class Note:
     def __init__(self):
         self.__note_list = list()
 
-    def add_person(self, person):
+    def add(self, person):
         self.__note_list.append(person)
 
-    def remove_person(self, index):
+    def remove(self, index):
         if 0 <= index <= len(self.__note_list)-1:
             self.__note_list.remove(self.__note_list[index])
 
-    def update_person(self, index, **kwargs):
+    def update(self, index, **kwargs):
         if 0 <= index <= len(self.__note_list)-1:
             person = self.__note_list[index]
             person.update(**kwargs)
@@ -35,19 +35,43 @@ class Note:
             result.append(i.get_info())
         return result
 
+
+class EmployeeNote(Note):
+
+    def nothing(self):
+        return 0
 n = Note()
+e = EmployeeNote()
 p1 = Person('Ivan', 24)
 p2 = Person('Ivan1', 23)
 p3 = Person('Ivan2', 25)
 p4 = Person('Ivan3', 26)
-n.add_person(p1)
-n.add_person(p2)
-n.add_person(p3)
-n.add_person(p4)
+print('Note')
+n.add(p1)
+n.add(p2)
+n.add(p3)
+n.add(p4)
 print(n.get_info_note())
 
-n.remove_person(2)
+n.remove(2)
 print(n.get_info_note())
 
-n.update_person(1, name='Jon', age=15, surname='RRR')
+n.update(1, name='Jon', age=15, surname='RRR')
 print(n.get_info_note())
+print('EmployeeNote')
+e.add(p1)
+e.add(p2)
+e.add(p3)
+e.add(p4)
+print(e.get_info_note())
+
+e.remove(2)
+print(e.get_info_note())
+
+e.update(1, name='Jon', age=15, surname='RRR')
+print(e.get_info_note())
+
+
+class Computer:
+    def __init__(self, processor, motherboard, graphic_card):
+        pass
